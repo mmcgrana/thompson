d56d2b9a5518ab1beff12370307fead121a0217b
Mark McGranaghan <mmcgrana@gmail.com>
2010-04-06 16:27:22 -0400

    Depend on Ring 0.2 final.

--- project.clj
+++ project.clj
@@ -6,10 +6,10 @@
   :javac-fork "true"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [ring/ring-core "0.2.0-SNAPSHOT"]
+                 [ring/ring-core "0.2.0"]
                  [clj-html "0.1.0"]]
-  :dev-dependencies [[ring/ring-jetty-adapter "0.2.0-SNAPSHOT"]
-                     [ring/ring-devel "0.2.0-SNAPSHOT"]
+  :dev-dependencies [[ring/ring-jetty-adapter "0.2.0"]
+                     [ring/ring-devel "0.2.0"]
                      [lein-clojars "0.5.0"]
                      [org.clojars.mmcgrana/lein-javac "0.1.0"]]
   :namespaces [thompson.web.handler
