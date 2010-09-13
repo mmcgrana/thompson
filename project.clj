105173a535c05d399dd2ea570133698d8ed1684e
Mark McGranaghan <mmcgrana@gmail.com>
2010-09-13 16:42:32 -0700

    update clojure and lein-javac deps

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :java-source-path "src"
   :clojure-source-path "src"
   :javac-fork "true"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [org.apache.commons/commons-math "2.1"]]
-  :dev-dependencies [[org.clojars.mmcgrana/lein-javac "0.1.0"]])
+  :dev-dependencies [[org.clojars.mmcgrana/lein-javac "1.2.1"]])
